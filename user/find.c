a569e362b2bc1191265ae4ac5e0f63a1b37aa207
slk000 <me@lo-li.net>
2021-12-16 23:47:08 +0800

    lab find

---
+++ user/find.c
@@ -0,0 +1,82 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+
+void
+find(char *path, char *pattern)
+{
+  // copied and modified from user/ls.c
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if((fd = open(path, 0)) < 0){
+    fprintf(2, "find: cannot open %s\n", path);
+    return;
+  }
+
+  if(fstat(fd, &st) < 0){
+    fprintf(2, "find: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  switch(st.type){
+  case T_FILE:
+    printf("%s is a file\n", path);
+    exit(1);
+
+  case T_DIR:
+    // len(path + '\' + dir entry name + \0)
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+      printf("find: path too long\n");
+      break;
+    }
+    strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+      if(de.inum == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      if(stat(buf, &st) < 0){
+        printf("find: cannot stat %s\n", buf);
+        continue;
+      }
+
+      switch(st.type){
+      case T_FILE:
+        if (strcmp(de.name, pattern) == 0){
+          printf("%s\n", buf);
+        }
+        break;
+      case T_DIR:
+        if (strcmp(de.name, "..") == 0 || strcmp(de.name, ".") == 0){
+          continue;
+        }
+        find(buf, pattern);
+        break;
+      }
+      
+
+    }
+    break;
+  }
+  close(fd);
+}
+
+int
+main(int argc, char *argv[])
+{
+    if (argc < 3) {
+        fprintf(2, "Usage: find path name\n");
+        exit(1);
+    }
+
+    find(argv[1], argv[2]);
+    exit(0);
+}
